ee3c3b2824d4b172ecdd7f623d8c802663516c97
JoseValdemar <104625882+JoseValdemar@users.noreply.github.com>
2025-05-15 21:46:11 +0100

    josev

--- ASIDMS/userms/src/main/resources/schema.sql
+++ ASIDMS/userms/src/main/resources/schema.sql
@@ -1,5 +1,10 @@
 CREATE TABLE IF NOT EXISTS users (
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE
+  userid BIGINT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255),
+  fullname VARCHAR(255),
+  shipping_order_id BIGINT,
+  username VARCHAR(255),
+  PRIMARY KEY (userid),
+  CONSTRAINT UK6dotkott2kjsp8vw4d0m25fb7 UNIQUE (email),
+  CONSTRAINT UKr43af9ap4edm43mmtq01oddj6 UNIQUE (username)
 );
